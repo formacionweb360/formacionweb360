49b8ce32a49e265070ee285cf8c1c6f04e8e2050
formacionweb360 <formacionweb360@gmail.com>
2026-03-03 09:53:43 -0500

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import LoginForm from "./components/LoginForm";
 import AdminPage from "./pages/AdminPage";
 import FormadorPage from "./pages/FormadorPage";
-//import FormadorAsistencia from "./pages/FormadorAsistencia"; //
+import { supabase } from "../services/supabaseClient";  // ✅ Ruta relativa correcta
 import AsesorDashboard from "./pages/AsesorDashboard";
 import CursoViewPage from "./pages/CursoViewPage";
 
